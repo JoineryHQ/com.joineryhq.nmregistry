3e318299b06f0b83a1706ea499e0eb7f2ff12137
Allen Shaw <allen@JoineryHQ.com>
2023-01-20 16:08:58 -0600

    Remove "contact summary overlay" column in profile search results.

--- js/CRM_Profile_Page_Listings-registrySearch.js
+++ js/CRM_Profile_Page_Listings-registrySearch.js
@@ -1,6 +1,11 @@
 CRM.$(function($) {
-  
+
   // Hide qill but don't hide mapping.
   $('div#search-status').after($('div#search-status i.crm-i.fa-map-marker').closest('a'));
   $('div#search-status').hide();
+
+  // Remove "contact summary overlay" column in profile search results.
+  // (These are alreayd hidden by css in the them, but let's just remove them entirely here too.)
+  $('div.crm-search-results > table > tbody > tr th:first').remove();
+  $('div.crm-search-results > table > tbody > tr td:first').remove();
 });

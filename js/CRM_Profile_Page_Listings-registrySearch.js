644db9f93abb4785402e5f66afa23b1c8fa99859
Allen Shaw <allen@JoineryHQ.com>
2023-01-22 21:28:26 -0600

    Registry search: clean up search results qill (remove extraneous and confusing info).

--- js/CRM_Profile_Page_Listings-registrySearch.js
+++ js/CRM_Profile_Page_Listings-registrySearch.js
@@ -1,8 +1,22 @@
 CRM.$(function($) {
 
-  // Hide qill but don't hide mapping.
-  $('div#search-status').after($('div#search-status i.crm-i.fa-map-marker').closest('a'));
-  $('div#search-status').hide();
+  if ($('div.crm-search-tasks').length) {
+    // On search WITH results:
+    // Hide qill but don't hide mapping.
+    $('div#search-status').after($('div#search-status i.crm-i.fa-map-marker').closest('a'));
+    $('div#search-status').hide();
+  }
+  else if ($('div.qill').closest('div.messages.status').length) {
+    // On search WITHOUT results:
+    // Clear the long explanation and just say "no results found."
+    var divMessagesStatus = $('div.qill').closest('div.messages.status');
+    var i = divMessagesStatus.find('i.crm-i:first').clone();
+    divMessagesStatus.empty();
+    divMessagesStatus.append(i);
+    divMessagesStatus.append('<span> ' + ts('No matches found.') + '</span>');
+    // Move the now-abbreviated search results block ABOVE the search criteria form.
+    $('div.crm-form-block').before(divMessagesStatus);
+  }
 
   // Remove "contact summary overlay" column in profile search results.
   // (These are alreayd hidden by css in the them, but let's just remove them entirely here too.)
